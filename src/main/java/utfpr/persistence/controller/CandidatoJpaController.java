81e50c6a355e86ed69eee57a30a80d54c65a8e88
Samsung <Samsung@Samsung-PC>
2014-03-24 15:14:35 -0300

    consulta com Criteria API

--- src/main/java/utfpr/persistence/controller/CandidatoJpaController.java
+++ src/main/java/utfpr/persistence/controller/CandidatoJpaController.java
@@ -5,12 +5,16 @@
 package utfpr.persistence.controller;
 
 import inscricao.persistence.entity.Candidato;
+import inscricao.persistence.entity.Candidato_;
 import inscricao.persistence.entity.Idioma;
+import inscricao.persistence.entity.Idioma_;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -39,11 +43,17 @@ public class CandidatoJpaController extends JpaController {
         EntityManager em = null;
         try {
             em = getEntityManager();
-            TypedQuery<Candidato> q = em.createQuery(
+            /*TypedQuery<Candidato> q = em.createQuery(
                 "select Candidato c from Candidato where c.idioma.codigo = :idioma",
                 Candidato.class);
-            q.setParameter("idioma", idioma);
+            q.setParameter("idioma", idioma);*/
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Candidato> cq = cb.createQuery(Candidato.class);
+            Root<Candidato> rt = cq.from(Candidato.class);
+            cq.where(cb.equal(rt.get(Candidato_.idioma).get(Idioma_.codigo), 1));
+            TypedQuery<Candidato> q = em.createQuery(cq);
             return q.getResultList();
+            
         } finally {
             if (em != null) em.close();
         }
